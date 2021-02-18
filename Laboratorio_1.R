83d3da6f3dff808dee85418998a0706c2bf72b11
Marco Tagle <marco.gonzaleztg@uanl.edu.mx>
2021-02-18 12:57:57 -0600

    update

--- Laboratorio_1.R
+++ Laboratorio_1.R
@@ -5,3 +5,32 @@
 # Gastos totales del estudiante
 
 300 + 240 + 1527 + 400 + 1500 + 1833
+
+celular <- 300
+
+celular * celular
+celular + 450
+
+
+gastos <- c(300, 240, 1527, 400, 1500, 1833)
+gastos
+
+sumatoria.total <- sum(gastos)
+
+gastos + 10
+
+gastos
+
+
+mean(gastos)
+
+# Listado de funciones de estadística descriptiva
+
+# mean es la media
+# var es la varianza
+# sd es la desviación estándar
+# median es la mediana
+
+var(gastos)
+sd(gastos)
+median(gastos)
